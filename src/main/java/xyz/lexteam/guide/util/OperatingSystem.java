a2822d7fe9156fd07d89b255e14fc7108d0aa537
Jamie Mansfield <dev@jamierocks.uk>
2016-01-28 17:57:53 +0000

    Add representation of OperatingSystems

---
+++ src/main/java/xyz/lexteam/guide/util/OperatingSystem.java
@@ -0,0 +1,85 @@
+/*
+ * This file is part of Guide, licensed under the MIT License (MIT).
+ *
+ * Copyright (c) 2016, Lexteam <http://www.lexteam.xyz/>
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+package xyz.lexteam.guide.util;
+
+import java.util.function.Predicate;
+
+/**
+ * Represents common operating systems.
+ *
+ * @author Jamie Mansfield
+ */
+public enum OperatingSystem implements Predicate<String> {
+
+    OSX("mac"),
+    LINUX("nix", "nux"),
+    WINDOWS("win"),
+    UNKNOWN;
+
+    private final String[] partials;
+
+    OperatingSystem(String... partials) {
+        this.partials = partials;
+    }
+
+    @Override
+    public boolean test(String s) {
+        for (String partial : this.partials) {
+            if (s.contains(partial)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public String getDataFolder() {
+        switch (this) {
+            case OSX:
+                return System.getProperty("user.home") + "/Library/Application Support";
+            case WINDOWS:
+                return System.getenv("APPDATA");
+            case LINUX:
+            case UNKNOWN:
+            default:
+                return System.getProperty("user.home");
+        }
+    }
+
+    /**
+     * Gets the operating system currently running on the user's system.
+     *
+     * @return The current {@link OperatingSystem}.
+     */
+    public static OperatingSystem getOs() {
+        String osName = System.getProperty("os.name").toLowerCase();
+
+        for (OperatingSystem os : values()) {
+            if (os.test(osName)) {
+                return os;
+            }
+        }
+
+        return OperatingSystem.UNKNOWN;
+    }
+}
